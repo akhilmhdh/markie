1107caf78f3b1a16a7ca7649fc85c7844c593606
akhilmhdh <akhilmhdh@gmail.com>
2020-03-13 16:11:07 +0530

    =testing...

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is Life
         </a>
       </header>
     </div>
