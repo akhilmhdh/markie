81bea88b1cfabfaa5bd9bc23957cf2c8a6f19782
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-16 10:53:31 +0000

    Deploy akhilmhdh/markie to github.com/akhilmhdh/markie.git:gh-pages

---
+++ precache-manifest.71416867695306630dc0220b465b0508.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8095f6cf90adea938505424a9698b68",
+    "url": "/markie/index.html"
+  },
+  {
+    "revision": "99640de8de195e8b2a84",
+    "url": "/markie/static/css/2.1f303d0b.chunk.css"
+  },
+  {
+    "revision": "06c58a6f1717d9af7b82",
+    "url": "/markie/static/css/main.c9901516.chunk.css"
+  },
+  {
+    "revision": "99640de8de195e8b2a84",
+    "url": "/markie/static/js/2.e1c7de49.chunk.js"
+  },
+  {
+    "revision": "760fd2c97441dc0122485851cab86362",
+    "url": "/markie/static/js/2.e1c7de49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c58a6f1717d9af7b82",
+    "url": "/markie/static/js/main.3e64eead.chunk.js"
+  },
+  {
+    "revision": "92b3bc3007b681e1609c",
+    "url": "/markie/static/js/runtime-main.f2793a17.js"
+  },
+  {
+    "revision": "5acbe25e263652b5629059193986a694",
+    "url": "/markie/static/media/download.5acbe25e.png"
+  },
+  {
+    "revision": "14e27f0efc3fc9f5019e0ac71368fdd7",
+    "url": "/markie/static/media/headlinerno.45_demo-webfont.14e27f0e.woff2"
+  },
+  {
+    "revision": "c8d54e28dcc399da5fa5470691fd5882",
+    "url": "/markie/static/media/headlinerno.45_demo-webfont.c8d54e28.woff"
+  },
+  {
+    "revision": "2f06121a8cc2d4ce1ae5f91ae093803a",
+    "url": "/markie/static/media/poppins-regular-webfont.2f06121a.woff2"
+  },
+  {
+    "revision": "b652b9c78185ad7f57aa0f43dd1ab4e8",
+    "url": "/markie/static/media/poppins-regular-webfont.b652b9c7.woff"
+  }
+]);
